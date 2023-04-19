284bccc17538fa8a207d9dfb3550fdebe3c82d4a
qkrwlgus718 <qkrwlgus718@naver.com>
2023-04-19 14:49:46 +0900

    test

--- crud/settings.py
+++ crud/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-ex-test-e9btb72blgn9664i.sel3.cloudtype.app']
 
 
 # Application definition
